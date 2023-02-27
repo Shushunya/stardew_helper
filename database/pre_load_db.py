dd02aaa7fc95b6dc8dd159f7c968f8345ec9217e
Oleksandra <mremarka314@gmail.com>
2023-02-27 22:31:55 +0200

    loading from csv file into db

---
+++ database/pre_load_db.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+from dateutil.parser import parse
+import csv
+
+import models as m
+
+engine = create_engine("sqlite:///csv_db.sqlite3")
+m.Base.metadata.create_all(engine)
+
+
+import pandas as pd
+
+data = pd.read_csv("seeds.csv")
+# print(data.dtypes)
+
+data.to_sql(con=engine, name=m.Seed.__tablename__, if_exists="append", index=False)
+
+session = sessionmaker(bind=engine)
+s = session()
+
+results = s.query(m.Seed).where(m.Seed.multiharvest==True)
+
+for r in results:
+    print(r)
