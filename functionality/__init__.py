035eef3ed134fe8232d858caf0dfa339c4b54ba5
Oleksandra <mremarka314@gmail.com>
2023-03-17 18:48:27 +0200

    added get_a_chain

--- functionality/__init__.py
+++ functionality/__init__.py
@@ -1,2 +1,2 @@
-from functionality.farming.funcs import get_all_items, pretty_output
+from functionality.farming.funcs import get_all_items, pretty_output, get_a_chain
 from functionality.farming.item_classes import *
